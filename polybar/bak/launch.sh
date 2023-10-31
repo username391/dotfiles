ca25149ba4876337f28755291f59c0e72e96577f
= <me@box>
2023-10-31 06:25:45 +0500

    old polybar config

---
+++ polybar/bak/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar mainbar &
+
+echo "Polybar загрузился..."
+(xdo id -m -N Polybar && polybar-msg cmd hide)&
+
