5edd92a8373c710884d25d3e065aed13914b9fbe
me <me@box>
2025-05-10 06:15:33 +0500

    Перенес скрипт в hypr

--- .config/scripts/random-wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename=`/bin/ls $1/ | xargs -n 1 basename | shuf -n 1`
-notify-send "wallpaper changed" "$filename" -i "image-jpeg" -a "wallpaper-change"
-
-hyprctl "hyprpaper preload $1/$filename"
-hyprctl "hyprpaper wallpaper ,$1/$filename"
