c7b3534d4bb14ef00dfa6a9304b8cc948b4f4a1b
me <me@box>
2025-05-10 05:58:18 +0500

    Скрипт для демонстрации текущего уровня звука в уведомлениях

---
+++ .config/scripts/show-volume.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Получаем текущую громкость (через pamixer)
+volume=$(pamixer --get-volume)
+mute=$(pamixer --get-mute)
+
+if [ "$mute" = "true" ]; then
+    notify-send.sh --replace-file 91191 "  Muted"
+else
+	if [ "$volume" -le 33 ]; then
+		notify-send.sh --replace-file 91191 " Volume: $volume%"
+	elif [ "$volume" -le 66 ]; then
+		notify-send.sh --replace-file 91191 " Volume: $volume%"
+	else
+		notify-send.sh --replace-file 91191 "  Volume: $volume%"
+	fi
+fi
