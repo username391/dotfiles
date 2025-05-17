2da3d92030e8d34460d9b63c37da94dd286d13cf
me <me@box>
2025-05-17 06:53:40 +0500

    Добавил бар

--- .config/scripts/show-volume.sh
+++ .config/scripts/show-volume.sh
@@ -1,18 +1,20 @@
-
 #!/bin/bash
 
 # Получаем текущую громкость (через pamixer)
 volume=$(pamixer --get-volume)
 mute=$(pamixer --get-mute)
 
+bar="   $(seq -s "─" $(($volume/4)) | sed 's/[0-9]//g')  "
+
 if [ "$mute" = "true" ]; then
-    notify-send.sh --replace-file ~/.cache/91191 "  Muted"
+    notify-send.sh --replace-file ~/.cache/91191 "     Muted\n$bar"
 else
+
 	if [ "$volume" -le 33 ]; then
-		notify-send.sh --replace-file ~/.cache/91191 " Volume: $volume%"
+		notify-send.sh --replace-file ~/.cache/91191 "    Volume: $volume% \n$bar"
 	elif [ "$volume" -le 66 ]; then
-		notify-send.sh --replace-file ~/.cache/91191 " Volume: $volume%"
+		notify-send.sh --replace-file ~/.cache/91191 "    Volume: $volume% \n$bar"
 	else
-		notify-send.sh --replace-file ~/.cache/91191 "  Volume: $volume%"
+		notify-send.sh --replace-file ~/.cache/91191 "     Volume: $volume% \n$bar"
 	fi
 fi
